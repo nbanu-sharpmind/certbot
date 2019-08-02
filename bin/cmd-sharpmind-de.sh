d8589cc0729f0763dd013dfdab0c27c88e744181
nbanu <nbanu@sharpmind.de>
2019-08-02 17:18:13 +0200

    fixed code to support wild-card domain (i.e *.sharpmind.de)

--- bin/cmd-sharpmind-de.sh
+++ bin/cmd-sharpmind-de.sh
@@ -1,3 +1,5 @@
+# for domain with wild-card (*) we need to add '--server https://acme-v02.api.letsencrypt.org/directory'
+
 BASE_DIR=/share/sharpmind/letsencrypt
 
 ../venv/bin/certbot certonly \
@@ -6,9 +8,10 @@ BASE_DIR=/share/sharpmind/letsencrypt
     --config-dir $BASE_DIR/config \
     --work-dir $BASE_DIR/work \
     --logs-dir $BASE_DIR/logs  \
+    --server https://acme-v02.api.letsencrypt.org/directory \
     --non-interactive \
     --agree-tos \
     --email is@sharpmind.de \
     --dns-checkdomain \
     --dns-checkdomain-credentials $BASE_DIR/key/checkdomain_key.ini \
-    --domain sharpmind.de
+    --domain '*.sharpmind.de'
